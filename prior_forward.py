f490ec9f6663a5955ba04cd35538b25b2795c3a0
Arnav <ec221038@qmul.ac.uk>
2023-08-11 12:22:11 -0400

    changing inference

--- prior_forward.py
+++ prior_forward.py
@@ -7,7 +7,7 @@ import argparse
 import math
 import torch.nn as nn
 from prior import Prior
-from prior.core import QuantizedNormal  
+from prior.core import QuantizedNormal, DiagonalShift
 from tqdm import tqdm
 
 def process_audio(input_path, output_path, model_checkpoint_path, pretrained_vae_path ,chunk_size):
@@ -19,7 +19,8 @@ def process_audio(input_path, output_path, model_checkpoint_path, pretrained_vae
     CYCLE_SIZE = 4
     N_LAYERS = 10
 
-    quantized_normal = QuantizedNormal(RESOLUTION)
+    # quantized_normal = QuantizedNormal(RESOLUTION)
+    # diagonal_shift = DiagonalShift()
 
     model = Prior(
         resolution=RESOLUTION,
@@ -38,41 +39,68 @@ def process_audio(input_path, output_path, model_checkpoint_path, pretrained_vae
     # Load audio file
     audio, sr = librosa.load(input_path)
 
-    # Split audio into chunks
-    chunks = librosa.util.frame(audio, frame_length=chunk_size, hop_length=2048).T
+    # # Split audio into chunks
+    # chunks = librosa.util.frame(audio, frame_length=chunk_size, hop_length=2048).T
 
-    # Initialize output array
-    output = []
+    # # Initialize output array
+    # output = []
 
-    # Pass each chunk through the model and reconstruct the signal
-    for i in tqdm(range(chunks.shape[0])):
-        # Get input chunk
-        chunk = chunks[None, i:i+1, :]
-        #chunk = np.expand_dims(chunk, 0)
-        x = torch.from_numpy(chunk)
+    # # Pass each chunk through the model and reconstruct the signal
+    # for i in tqdm(range(chunks.shape[0])):
+    #     # Get input chunk
+    #     chunk = chunks[None, i:i+1, :]
+    #     x = torch.from_numpy(chunk)
 
-        # Pass chunk through model and get output
-        with torch.no_grad():
-            print("x.shape:", x.shape)
-            x_encoded = model.encode(x)
-            print("x_encoded.shape:", x_encoded.shape)
-            x_encoded = quantized_normal.encode(x_encoded)
-            y = model.forward(x_encoded)
-            y = quantized_normal.decode(y)
-            print("y.shape pre slice:", y.shape)
-            y = y[:, :, -1:None]
-            print("y.shape post slice:", y.shape)
-            y = model.decode(y)
+    #     # Pass chunk through model and get output
+    #     with torch.no_grad():
+    #         # x_encoded = model.encode(x)
+    #         # x_encoded = model.quantized_normal.encode(x_encoded)
+    #         # y = model.forward(x_encoded)
+    #         # y = model.post_process_prediction(y)
+    #         # y = model.quantized_normal.decode(y)
+    #         # y = y[:, :, -1:None]
+    #         # y = model.decode(y)
+    #         x_encoded = model.encode(x)
+    #         print(x_encoded.shape)
+    #         x_encoded = model.quantized_normal.encode(x_encoded)
+    #         z = model.generate(x_encoded)
 
-        # Store output
+    #         z = model.quantized_normal.decode(z)
+
+    #         y = model.decode(z)
+    #         # Store output
         
-        output.append(y.detach().numpy())
+    #         output.append(y.detach().numpy())
 
-    # Reconstruct signal from output chunks
-    output = np.concatenate(output, axis=-1)[0]
+    # # Reconstruct signal from output chunks
+    # output = np.concatenate(output, axis=-1)[0]
+    x_encoded = model.encode(torch.from_numpy(audio)[None, None, :])
+    x_encoded = model.quantized_normal.encode(x_encoded)
+    preds = []
+    for i in tqdm(range(x_encoded.shape[-1] - 1)):
+        start = None
+
+        pred = model.forward(x_encoded[..., start:i + 1])
+
+        pred = model.post_process_prediction(pred)
+        preds.append(pred[..., -1:None])
+
+    preds = torch.cat(preds, dim=0)
+    print("preds shape", preds.shape)
+    z = model.quantized_normal.decode(preds)
+
+    y = model.decode(z)
+    # Store output
+
+    print("y shape", y.shape)
+    y=y.reshape(1,-1)
+    print("y shape post reshape", y.shape)
+
+    output = y.squeeze().detach().numpy()
+
 
     # Save the output audio to the output folder using soundfile
-    sf.write(output_path, output.T, sr)
+    sf.write(output_path, output, sr)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process audio using specified parameters.")
@@ -80,7 +108,7 @@ if __name__ == "__main__":
     parser.add_argument("--output_path", type=str, default="output.wav", help="Path to save the output processed audio.")
     parser.add_argument("--model_checkpoint_path", type=str, help="Path to the model checkpoint.", required=True)
     parser.add_argument("--pretrained_vae_path", type=str,  help="Path to the pre-trained VAE checkpoint.", required=True)
-    parser.add_argument("--chunk_size", type=int, default=2048*32, help="Chunk size for processing the audio.")
+    parser.add_argument("--chunk_size", type=int, default=2048*8, help="Chunk size for processing the audio.")
 
     args = parser.parse_args()
 
